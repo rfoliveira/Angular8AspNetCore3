6fba6de80ebcf632f893547d2437f09b55062bf6
Rafael Fernandes de Oliveira <rafaelolivei@gmail.com>
2020-02-01 13:37:14 -0300

    Subindo fontes

---
+++ ViewModels/FeedViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular8AspNetCore3.ViewModels
+{
+    public class FeedViewModel
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string FeedType { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime PubDate { get; set; }
+        public string PublishDate { get; set; }
+
+        public FeedViewModel()
+        {
+            Link = "";
+            Title = "";
+            FeedType = "";
+            Author = "";
+            Content = "";
+            PubDate = DateTime.Today;
+            PublishDate = DateTime.Today.ToString("dd-MMM-yyyy");
+        }
+
+    }
+}
